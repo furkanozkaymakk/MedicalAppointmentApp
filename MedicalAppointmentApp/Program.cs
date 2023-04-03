321fb96e28cde1cfcd4ff3e407fc4dd57065b413
furkanozkaymakk <74023824+furkanozkaymakk@users.noreply.github.com>
2023-04-03 16:59:06 +0300

    ana sayfanın türkçeleştirilmesi

--- MedicalAppointmentApp/Program.cs
+++ MedicalAppointmentApp/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Razor sayfa yenileme için
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
