e75ffbf53678b40a7b409ce84b308757fbd39a5d
furkanozkaymakk <74023824+furkanozkaymakk@users.noreply.github.com>
2023-04-17 12:36:33 +0300

    Login Code

---
+++ MedicalAppointmentApp/Areas/Admin/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedicalAppointmentApp.Areas.Admin.Controllers
+{
+	public class DefaultController : Controller
+	{
+		[Route("Admin/[controller]/[action]/{id?}")]
+		[Route("Admin/[controller]/[action]/")]
+		[Area("Admin")]
+		[AllowAnonymous]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
